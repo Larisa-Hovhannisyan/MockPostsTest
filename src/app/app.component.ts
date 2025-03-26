af26a0c4f623e7b9726df69267f50820eb15dc1a
Larisa Hovhannisyan <larisahovhannisyan70@gmail.com>
2025-03-27 00:58:04 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myProject';
+  posts: any[] = [];
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.postService.getPosts().subscribe(data => {
+      this.posts = data;
+    });
+  }
 }
